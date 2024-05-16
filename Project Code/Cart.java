03b502cd7eb22bb054a5cea0666e0e8340fa7dea
ProkopisKamatsos <162447971+ProkopisKamatsos@users.noreply.github.com>
2024-05-16 12:53:54 +0300

    Create Cart.java

---
+++ Project Code/Cart.java
@@ -0,0 +1,23 @@
+public class Cart {
+    // Add attributes and methods as needed
+
+    public Cart() {
+        // Implementation for cart constructor
+    }
+
+    public void addItem() {
+        // Implementation for adding item to cart
+    }
+
+    public void removeItem() {
+        // Implementation for removing item from cart
+    }
+
+    public void viewItems() {
+        // Implementation for viewing items in cart
+    }
+
+    public void checkout() {
+        // Implementation for checkout
+    }
+}

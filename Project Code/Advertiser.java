46f9315bb97cdebc184999da45d74a4fcaa0b41d
ProkopisKamatsos <162447971+ProkopisKamatsos@users.noreply.github.com>
2024-05-16 12:53:07 +0300

    Update Advertiser.java

--- Project Code/Advertiser.java
+++ Project Code/Advertiser.java
@@ -1,41 +1,38 @@
 public class Advertiser extends User {
     private int adDuration;
     private double offeredMoney;
-    private Product product;
+    private String product;
 
-    public Advertiser(int userID, String username, String password, String email, int adDuration, double offeredMoney, Product product) {
-        super(userID, "Advertiser", username, password, email);
+    public Advertiser(int userId, String userType, String username, String password, String email, int adDuration, double offeredMoney, String product) {
+        super(userId, userType, username, password, email);
         this.adDuration = adDuration;
         this.offeredMoney = offeredMoney;
         this.product = product;
     }
 
-    // Getters and Setters
-    public int getAdDuration() {
-        return adDuration;
-    }
+    // Getters
+    public int getAdDuration() { return adDuration; }
+    public double getOfferedMoney() { return offeredMoney; }
+    public String getProduct() { return product; }
 
-    public void setAdDuration(int adDuration) {
-        this.adDuration = adDuration;
-    }
-
-    public double getOfferedMoney() {
-        return offeredMoney;
-    }
-
-    public void setOfferedMoney(double offeredMoney) {
-        this.offeredMoney = offeredMoney;
-    }
-
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
-        this.product = product;
-    }
+    // Setters
+    public void setAdDuration(int adDuration) { this.adDuration = adDuration; }
+    public void setOfferedMoney(double offeredMoney) { this.offeredMoney = offeredMoney; }
+    public void setProduct(String product) { this.product = product; }
 
     public void sendRequest() {
-        // logic to send request
+        System.out.println("Advertiser: Request sent.");
+    }
+
+    public void fillForm() {
+        System.out.println("Advertiser: Form filled.");
+    }
+
+    public void contact() {
+        System.out.println("Advertiser: Contact made.");
+    }
+
+    public void myRequests() {
+        System.out.println("Advertiser: Viewing my requests.");
     }
 }

75a8ef42125bd70b0177015818d2aca408eefac6
ProkopisKamatsos <162447971+ProkopisKamatsos@users.noreply.github.com>
2024-05-16 12:52:31 +0300

    Update Admin.java

--- Project Code/Admin.java
+++ Project Code/Admin.java
@@ -1,14 +1,37 @@
 public class Admin extends User {
-
-    public Admin(int userID, String username, String password, String email) {
-        super(userID, "Admin", username, password, email);
+    public Admin(int userId, String userType, String username, String password, String email) {
+        super(userId, userType, username, password, email);
     }
 
-    public void approveTrackRequest() {
-        // logic to approve track request
+    public void addTrack() {
+        System.out.println("Admin: Track added.");
     }
 
-    public void approveAdRequest() {
-        // logic to approve ad request
+    public void removeTrack() {
+        System.out.println("Admin: Track removed.");
+    }
+
+    public void updateTrack() {
+        System.out.println("Admin: Track updated.");
+    }
+
+    public void addAd() {
+        System.out.println("Admin: Ad added.");
+    }
+
+    public void removeAd() {
+        System.out.println("Admin: Ad removed.");
+    }
+
+    public void updateAd() {
+        System.out.println("Admin: Ad updated.");
+    }
+
+    public void processRequest() {
+        System.out.println("Admin: Request processed.");
+    }
+
+    public void viewReservations() {
+        System.out.println("Admin: Viewing reservations.");
     }
 }

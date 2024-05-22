82166847e463bd42dc209f7cebc88b4be75c9bf2
ProkopisKamatsos <162447971+ProkopisKamatsos@users.noreply.github.com>
2024-05-22 17:22:09 +0300

    Update Cart.java
    make product.java for the list

--- Παραδοτέο 3ο/ProjectCode/Cart.java
+++ Παραδοτέο 3ο/ProjectCode/Cart.java
@@ -1,18 +1,9 @@
 public class Cart {
-    private List<Product> products;
     private double totalPrice;
 
   
 
-    // Getters and Setters
-    public List<Product> getProducts() {
-        return products;
-    }
 
-    public void setProducts(List<Product> products) {
-        this.products = products;
-        calculateTotalPrice();
-    }
 
     public double getTotalPrice() {
         return totalPrice;

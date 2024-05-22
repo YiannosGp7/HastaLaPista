3de7ead2349e59c3f3d466b97da8c2b14fe16848
XouselasNikolaos <85506620+XouselasNikolaos@users.noreply.github.com>
2024-05-22 18:06:41 +0300

    Update email.java

--- Παραδοτέο 3ο/ProjectCode/email.java
+++ Παραδοτέο 3ο/ProjectCode/email.java
@@ -30,3 +30,21 @@ public class Email {
 
     public String getSubject() {
         return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void sendEmail() {
+        System.out.println("Email sent to: " + this.reservation.getRenter().getEmail() + " with subject: " + this.subject);
+    }
+}

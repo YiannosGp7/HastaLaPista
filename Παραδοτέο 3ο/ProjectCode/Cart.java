d75f69e4cb5791dc02bc88cc77c0c8e90356ed24
ProkopisKamatsos <162447971+ProkopisKamatsos@users.noreply.github.com>
2024-05-22 14:42:07 +0300

    Create Cart.java
    cart only getters setters

---
+++ Παραδοτέο 3ο/ProjectCode/Cart.java
@@ -0,0 +1,28 @@
+public class Cart {
+    private List<Product> products;
+    private double totalPrice;
+
+    // Constructor
+    public Cart() {
+        this.products = new ArrayList<>();
+        this.totalPrice = 0.0;
+    }
+
+    // Getters and Setters
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+        calculateTotalPrice();
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}

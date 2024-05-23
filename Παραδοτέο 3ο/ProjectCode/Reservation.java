51990f849a63de907ecaa04bb3f8f1066627d8f7
YiannosGp7 <giannispantelis07@gmail.com>
2024-05-23 12:42:26 +0300

    Update Reservation.java
    add getters-setters in reservation.java

--- Παραδοτέο 3ο/ProjectCode/Reservation.java
+++ Παραδοτέο 3ο/ProjectCode/Reservation.java
@@ -1,5 +1,7 @@
 import java.util.Date;
 
+import javax.sound.midi.Track;
+
 public class Reservation {
     private int reservationID;
     private Date reservationDateTime;
@@ -12,3 +14,37 @@ public class Reservation {
         this.renter = renter;
         this.track = track;
     }
+
+    // Getters and Setters
+    public int getReservationID() {
+        return reservationID;
+    }
+
+    public void setReservationID(int reservationID) {
+        this.reservationID = reservationID;
+    }
+
+    public Date getReservationDateTime() {
+        return reservationDateTime;
+    }
+
+    public void setReservationDateTime(Date reservationDateTime) {
+        this.reservationDateTime = reservationDateTime;
+    }
+
+    public Renter getRenter() {
+        return renter;
+    }
+
+    public void setRenter(Renter renter) {
+        this.renter = renter;
+    }
+
+    public Track getTrack() {
+        return track;
+    }
+
+    public void setTrack(Track track) {
+        this.track = track;
+    }
+}

93fcd134318611871f7760d5d4eaafbacc01bd56
YiannosGp7 <giannispantelis07@gmail.com>
2024-05-24 11:24:19 +0300

    Update payment.java 2
    add getters and setters for payment class

--- Παραδοτέο 3ο/ProjectCode/Payment.java
+++ Παραδοτέο 3ο/ProjectCode/Payment.java
@@ -10,3 +10,43 @@ public class Payment {
         this.amount = amount;
         this.paymentMethod = paymentMethod;
     }
+
+
+    // Getters and Setters
+    public int getPaymentID() {
+        return paymentID;
+    }
+
+    public void setPaymentID(int paymentID) {
+        this.paymentID = paymentID;
+    }
+
+    public Renter getRenter() {
+        return renter;
+    }
+
+    public void setRenter(Renter renter) {
+        this.renter = renter;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public void confirmPayment() {
+        System.out.println("Payment confirmed for: " + this.renter.getUsername());
+    }
+}
+

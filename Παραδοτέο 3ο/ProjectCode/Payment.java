4d6e346a8e72a0d3b36c66460f36879aa7ee3fb4
YiannosGp7 <giannispantelis07@gmail.com>
2024-05-24 11:23:16 +0300

    Update payment.java
    add variables and methods

--- Παραδοτέο 3ο/ProjectCode/Payment.java
+++ Παραδοτέο 3ο/ProjectCode/Payment.java
@@ -1,3 +1,12 @@
 public class Payment {
-    
-}
+    private int paymentID;
+    private Renter renter;
+    private double amount;
+    private String paymentMethod;
+
+    public Payment(int paymentID, Renter renter, double amount, String paymentMethod) {
+        this.paymentID = paymentID;
+        this.renter = renter;
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+    }

c947724c7788025ddfffe72d76fcdebed442bf31
Alexthm21 <149508824+Alexthm21@users.noreply.github.com>
2024-05-24 14:08:12 +0300

    Update TrackOwner.java

--- Παραδοτέο 3ο/ProjectCode/TrackOwner.java
+++ Παραδοτέο 3ο/ProjectCode/TrackOwner.java
@@ -12,3 +12,35 @@ public class TrackOwner extends User {
         this.trackEquipment = trackEquipment;
         this.trackVehicles = trackVehicles;
         this.trackLocation = trackLocation;
+}
+
+    // Getters
+    public int getTrackLength() { return trackLength; }
+    public double getTrackPrice() { return trackPrice; }
+    public String getTrackEquipment() { return trackEquipment; }
+    public String getTrackVehicles() { return trackVehicles; }
+    public String getTrackLocation() { return trackLocation; }
+
+    // Setters
+    public void setTrackLength(int trackLength) { this.trackLength = trackLength; }
+    public void setTrackPrice(double trackPrice) { this.trackPrice = trackPrice; }
+    public void setTrackEquipment(String trackEquipment) { this.trackEquipment = trackEquipment; }
+    public void setTrackVehicles(String trackVehicles) { this.trackVehicles = trackVehicles; }
+    public void setTrackLocation(String trackLocation) { this.trackLocation = trackLocation; }
+
+    public void sendRequest() {
+        System.out.println("TrackOwner: Request sent.");
+    }
+
+    public void fillForm() {
+        System.out.println("TrackOwner: Form filled.");
+    }
+
+    public void contact() {
+        System.out.println("TrackOwner: Contact made.");
+    }
+
+    public void myRequests() {
+        System.out.println("TrackOwner: Viewing my requests.");
+    }
+}

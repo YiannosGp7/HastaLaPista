46c21e1a0e782d708d73248f19110d8ecfa4f7ab
YiannosGp7 <giannispantelis07@gmail.com>
2024-05-24 12:06:55 +0300

    Update Main.java
    add admin actions

--- Παραδοτέο 3ο/ProjectCode/Main.java
+++ Παραδοτέο 3ο/ProjectCode/Main.java
@@ -5,5 +5,29 @@ public class Main {
         TrackOwner trackOwner = new TrackOwner(2, "trackOwner", "ownerUser", "ownerPass", "owner@example.com", 500, 200.0, "Basic Equipment", "Cars", "Location A");
         Renter renter = new Renter(3, "renter", "renterUser", "renterPass", "renter@example.com", 30);
         Advertiser advertiser = new Advertiser(4, "advertiser", "advertUser", "advertPass", "advert@example.com", 30, 500.0, "Product A");
+    
+
+        // Admin actions
+        System.out.println("Admin actions:");
+        admin.addTrack();
+        admin.removeTrack();
+        admin.updateTrack();
+        admin.addAd();
+        admin.removeAd();
+        admin.updateAd();
+        admin.processRequest();
+        admin.viewReservations();
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
 }

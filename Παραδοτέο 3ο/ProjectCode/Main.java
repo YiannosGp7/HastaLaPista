66df9c71fa71787e61df7d58d26b7c4fc94ebb8e
YiannosGp7 <giannispantelis07@gmail.com>
2024-05-24 12:03:09 +0300

    Create Main.java
    Create main.java

---
+++ Παραδοτέο 3ο/ProjectCode/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Creating users
+        Admin admin = new Admin(1, "admin", "adminUser", "adminPass", "admin@example.com");
+        TrackOwner trackOwner = new TrackOwner(2, "trackOwner", "ownerUser", "ownerPass", "owner@example.com", 500, 200.0, "Basic Equipment", "Cars", "Location A");
+        Renter renter = new Renter(3, "renter", "renterUser", "renterPass", "renter@example.com", 30);
+        Advertiser advertiser = new Advertiser(4, "advertiser", "advertUser", "advertPass", "advert@example.com", 30, 500.0, "Product A");
+    }
+}
